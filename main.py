c4fe4e91a5391a99dccb88966272ec07e637560c
Matheus Mendonça <lopesmatheus642@gmail.com>
2023-09-13 08:10:42 -0300

    feat: checkbox can now be added

--- main.py
+++ main.py
@@ -14,7 +14,16 @@ class MyScrollableCheckboxFrame(ctk.CTkScrollableFrame):
         #     checkbox.grid(row=i, column=0, padx=10, pady=(10, 0), sticky="w")
         #     self.checkboxes.append(checkbox)
             
-
+    def add_checkbox(self):
+        dialog = ctk.CTkInputDialog(title="New Checkbox", text="Type in the name and due date: ")
+        checkbox_text = dialog.get_input()
+        if checkbox_text:
+            checkbox = ctk.CTkCheckBox(self, text=checkbox_text)
+            checkbox.grid(row=len(self.checkboxes), column=0, padx = 10, pady=(10,0), sticky="w")
+            self.checkboxes.append(checkbox)
+            self.get()
+        
+    
     def get(self):
         checked_checkboxes = []
         for checkbox in self.checkboxes:
@@ -40,18 +49,20 @@ class App(ctk.CTk):
         #UI elements
         self.assignments_frame = MyScrollableCheckboxFrame(self, title="Assignments")
         self.assignments_frame.grid(row=0, column=0, padx=10, pady=(10,0), sticky="nsew")
-        self.assignments_button = ctk.CTkButton(self, text="Add Assignment")
+        self.assignments_button = ctk.CTkButton(self, text="Add Assignment", command=self.assignments_frame.add_checkbox)
         self.assignments_button.grid(row=3, column=0, padx=10, pady=10, sticky="nsew")
         
         self.tests_frame = MyScrollableCheckboxFrame(self, title="Tests")
         self.tests_frame.grid(row=0, column=1, padx=(0,10), pady=(10,0),sticky="nsew")
-        self.tests_button = ctk.CTkButton(self, text="Add Test")
+        self.tests_button = ctk.CTkButton(self, text="Add Test",command=self.tests_frame.add_checkbox)
         self.tests_button.grid(row=3, column=1, padx=(0,10), pady=10, sticky="nsew")
             
         self.house_frame = MyScrollableCheckboxFrame(self, title="House Chores")
         self.house_frame.grid(row=0, column=2, padx=(0,10), pady=(10,0), sticky="nsew")
-        self.house_button = ctk.CTkButton(self, text="Add House Chore")
+        self.house_button = ctk.CTkButton(self, text="Add House Chore", command=self.house_frame.add_checkbox )
         self.house_button.grid(row=3, column=2, padx=(0,10), pady=10, sticky="nsew")
+        
+        
         
 app = App()
 app.mainloop()
